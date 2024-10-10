7ad202d3b8801bb4e38e5d72a3c9e9656f63a53b
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2024-10-10 12:41:55 +0200

    init code

---
+++ knikkerbaan/knikkerbaan.ino
@@ -0,0 +1,43 @@
+// globale variabelen
+int geteldeKnikkers = 0;
+unsigned long huidigeTijd = 0;
+unsigned long laatsteSerialPrintTijd = 0;
+
+// toestand(en)
+const int TOESTAND_SERIAL_PRINT_NU = 1;
+const int TOESTAND_SERIAL_WACHT = 0
+int toestandSerial = TOESTAND_SERIAL_WACHT;
+
+
+// constanten voor pinnen
+const int TELLERPIN = 3;
+
+// overige constanten
+const int SERIAL_PRINT_INTERVAL = 1000;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(tellerPin, INPUT);
+}
+
+void loop() {
+  // lees sensoren uit
+  huidigeTijd = millis();
+
+  // verander toestanden op basis van sensoren
+  if (huidigeTijd - laatsteSerialPrintTijd > SERIAL_PRINT_INTERVAL) {
+    toestandSerial = TOESTAND_SERIAL_PRINT_NU;
+  }
+
+  // pas actuatoren aan
+  if (toestandSerial == TOESTAND_SERIAL_PRINT_NU) {
+    Serial.println("getelde knikkers: " + geteldeKnikkers);
+  }
+
+  // verander toestand indien een actuator maar eenmalig iets moet doen
+  if (toestandSerial == TOESTAND_SERIAL_PRINT_NU) {
+    toestandSerial = TOESTAND_SERIAL_PRINT_WACHT;
+  }
+}
